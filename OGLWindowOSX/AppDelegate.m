dcc938f7d5f54ccc4eaf0d6ee28704d391327fc3
kenny <kenny@tradexport.com>
2021-07-24 23:42:39 +0800

    Initial Commit

---
+++ OGLWindowOSX/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  OGLWindowOSX
+//
+//  Created by zhaozhenhui on 2021/7/24.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ OGLWindowOSX/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OGLWindowOSX
+//
+//  Created by zhaozhenhui on 2021/7/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
